7f3f82d6adb0479d0ecdbaf52daf1df948a81c50
eesypeesy <46086523+eesypeesy@users.noreply.github.com>
2020-06-29 23:25:34 +0530

    dfd

---
+++ app.js
@@ -0,0 +1,5 @@
+function startCaptcha() {
+    document.getElementById("captchaButton").style.display = "block";
+}function onSubmit(token) {
+    document.getElementById("mail").style.display = "block";
+  }
